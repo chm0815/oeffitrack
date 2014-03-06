f6ec998c71594c0a71c9f8bd705c4eb37aa5e516
chm0815 <christoph_m@gmx.net>
2014-03-06 20:24:45 +0100

    configure codeigniter and custom oeffitrack settings

---
+++ web/application/config/oetconfig.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+  $config['oet_version'] = '1.0 DEV';
+  $config['oet_h1'] = 'Oeffitrack Demo - Version '.$config['oet_version'];
+
+  // googlemaps or openlayers
+  $config['oet_mapapi'] = 'googlemaps';//'googlemaps';
+
+  //$config['oet_mapapikey'] = 'ABQIAAAAQm226KlKdrMHcrSP9sdxLxQrq-Ndf1GrLz98GADynUASDyV02hSOv1zkOMbYGKiDXCrWR0lk1G20zQ';
+  $config['oet_mapapikey'] = 'ABQIAAAAYicwjjqGNtWxK3JGcVOxwxTE20y9UNhcBun2UkQ7-2Mxa11U5hRAKUUnbg-TT3wQtY4f7Hq7DeK1eg';
+
+  // google map zoom on homepage 
+  $config['oet_zoom'] = 10;
+  $config['oet_lat'] = 47.07;
+  $config['oet_lon'] = 15.99;
+?>
