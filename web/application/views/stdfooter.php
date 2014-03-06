c8db6a47bf2c1de1ebc1f932b2d5b0759329ce70
chm0815 <christoph_m@gmx.net>
2014-03-07 00:31:51 +0100

    first view parts and main controller file

---
+++ web/application/views/stdfooter.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+		</div><!--End Content-->
+	</div><!--End Wrapper-->
+	<div id="footer">
+		&copy; 2014 Concept and Development by Maurer Christoph 
+	</div><!--End Footer-->
+</div><!--End Container-->
+</body></html>
---
+++ web/application/views/stdheader.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?><!DOCTYPE html><head>
+<title><?php echo config_item('oet_h1');?></title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/std.css">
+</head>
+<div id="container">
+  <div id="header">
+    <h1><?php echo config_item('oet_h1');?></h1>
+  </div><!--End Header-->
+    <div id="nav">
+      <ul>
+        <li><a href="<?php echo base_url();?>">Overview
+        </a></li><li><a href="<?php echo base_url();?>main/routes">Routes
+        </a></li><li><a href="#">Login
+        </a></li></ul>
+    </div><!--End nav-->
+  <div id="wrapper">
+    <div id="content">
