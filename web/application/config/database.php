6b504d23fa43b509ac1d517e35ef470e1ddf7562
chm0815 <christoph_m@gmx.net>
2014-03-06 20:30:55 +0100

    default database connection

--- web/application/config/database.php
+++ web/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'oet';
+$db['default']['password'] = '1234'; // CHANGE PW on production systems !!!
+$db['default']['database'] = 'oet2';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
