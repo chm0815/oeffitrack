82b118412ca9c4f6cbe8471abbed0e4c089daddb
chm0815 <christoph_m@gmx.net>
2014-03-10 23:12:38 +0100

    businformation diff actual target values

---
+++ web/application/models/businformation.php
@@ -0,0 +1,37 @@
+<?php
+
+  class Businformation extends Model
+  {
+
+    function getActualTarget($routeid, $datetime)
+    {
+      $sql = "select rs.stopnr as stopnr,rs.stoptime as stoptime,time(al.logtime) as logtime,rs.name as name,
+          time_to_sec(subtime(time(al.logtime),rs.stoptime)) as diff
+          from routestations rs
+          left outer join arrivelogs al
+          on rs.routeid=al.routeid and rs.stopnr=al.stopnr and date(al.logtime)=date( ? ) and al.logtime<= ?
+          where rs.routeid= ?";
+      $query = $this->db->query($sql, array($datetime, $datetime, (int)$routeid));
+      $rows = $query->result_array();
+      return $rows;
+    }
+    
+    function getDiff($routeid, $datetime)
+    {
+      $rv = null;
+      $sql = "select timediff(time(logtime),time( ? )) as diff,logtime,stoptime from arrivelogs 
+          where routeid= ? and date(logtime)=date( ? ) 
+          order by logtime desc limit 1";
+      $query = $this->db->query($sql, array($datetime, (int)$routeid, $datetime));
+      
+      $row = $query->result_array();
+      if (count($row)==1) {
+        $rv = $row[0];
+      }
+      return $rv;
+    }
+    
+  }
+
+
+?>
