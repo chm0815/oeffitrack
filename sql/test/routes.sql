39da4c848d9f363edbe645bc717d879bf562120f
chm0815 <christoph_m@gmx.net>
2014-03-10 19:00:26 +0100

    test data

---
+++ sql/test/routes.sql
@@ -0,0 +1,28 @@
+insert into routes values (2,'Nestelbach-Großwilfersdorf');
+/*nestelbach*/
+insert into routepoints (id,routeid,busstopid,stopnr,stoptime)
+values (1,2,9,1,'09:10');
+
+/*mutzenfeld*/
+insert into routepoints (id,routeid,busstopid,stopnr,stoptime)
+values (2,2,8,2,'09:12');
+
+/*doerfl*/
+insert into routepoints (id,routeid,busstopid,stopnr,stoptime)
+values (3,2,7,3,'09:14');
+
+/*ilz*/
+insert into routepoints (id,routeid,busstopid,stopnr,stoptime)
+values (4,2,6,4,'09:15');
+
+/*neudorf*/
+insert into routepoints (id,routeid,busstopid,stopnr,stoptime)
+values (5,2,5,5,'09:16');
+
+/*hainfeld*/
+insert into routepoints (id,routeid,busstopid,stopnr,stoptime)
+values (6,2,4,6,'09:18');
+
+/*grosswilfersdorf*/
+insert into routepoints (id,routeid,busstopid,stopnr,stoptime)
+values (7,2,3,7,'09:20');
