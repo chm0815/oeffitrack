533e07727574a75e3a2921c29fc93a8bb07b0b09
chm0815 <christoph_m@gmx.net>
2014-03-25 18:03:56 +0100

    calculate distance

--- web/application/views/logtool.php
+++ web/application/views/logtool.php
@@ -186,19 +186,50 @@ $(document).ready(function() {
 
 var logging;
 var logtimer;
+var routestations;
+
+//------------------------------------------------------------------------------
+// http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+function getDistanceFromLatLonInMeter(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1); 
+  var a = 
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+  var d = R * c; // Distance in km
+  return d * 1000.0;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+//------------------------------------------------------------------------------
+
+
+function initRouteStations(data)
+{
+  routestations = new Array();
+  $.each(data, function(i, val) {
+    var pos = val;
+    pos.logged = false;
+    routestations.push(pos);
+  });
+}
+
 
 $(document).ready(function() {
   logging = false;
+  var routeid = <?php echo $route['id'];?>;
+  $.getJSON( "/route/routestations/" + routeid)
+    .done(function(data) {
+      initRouteStations(data);
+    }
+  );
   
-  //~ if (bus_marker == null) {
-    //~ bus_marker = new google.maps.Marker({
-      //~ position: new google.maps.LatLng(val.lat, val.lon),
-      //~ map: map,
-      //~ title: "route: " + routeid ,
-      //~ icon: "/img/bus2.png"
-    //~ });
-  //~ }
-
   $("#start_button").click(function() {
     if (logging) {
       logging = false;
